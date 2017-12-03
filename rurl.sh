0a0c2b237461ac47fbaa8407a0407faaf288b359
Cj <cj-bc@y-modify.org>
2017-12-03 22:28:43 +0900

    Add detection of non-arg($# -eq 0)
    now, with no arg rurl return its version

--- rurl.sh
+++ rurl.sh
@@ -7,11 +7,16 @@
 # This software is released under MIT License.
 # http://opensource.org/license/mit-license.php
 # version 0.0.1
+version=0.0.1
 
 source .sourcefiles
 
-
-case $1 in
-   "show" ) shift;show;;
-   * ) saveurl $@;;
-esac
+if [ $# -ne 0 ]
+then
+  case $1 in
+     "show" ) shift;show;;
+     * ) saveurl $@;;
+  esac
+else
+  echo "rURL version:"$version
+fi
